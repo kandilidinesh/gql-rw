065a03a5b7e0112ea7c8a61333a48feca8397ddc
kandilidinesh <kandilidinesh@gmail.com>
2020-07-07 10:10:54 +0530

    Resolver Function Arguments

--- src/index.js
+++ src/index.js
@@ -6,26 +6,48 @@ const {ApolloServer, gql} = require('apollo-server-express');
 
 const app = express();
 
+let users = {
+    1: {
+        id: '1',
+        username: 'Dinesh'
+    },
+    2:{
+        id:'2',
+        username: 'Magesh'
+    }
+};
+
+// const me = users[2];
+
 const schema = gql`
     type Query{
         me: User
+        user(id: ID!): User
+        users:[User!]
     }
 
     type User{
-        username: String
+        id: ID!
+        username: String!
     }
 `;
+
+//In JavaScript, the resolvers are grouped in a JavaScript object, often called a resolver map.
 const resolvers = {
     Query: {
-        me: () => {
-            return {
-                username: 'Dinesh K N'
-            };
+        me: (parent, args, {me}) => {return me;},
+        user: (parent, {id}) =>{
+            return users[id];
+        },
+        users:() => {
+            return Object.values(users);
         }
     }
 };
 
-const server = new ApolloServer({typeDefs:schema, resolvers});
+const server = new ApolloServer({typeDefs:schema, resolvers, context:{
+    me: users[1]
+}});
 
 server.applyMiddleware({ app, path: '/graphql' });
 
